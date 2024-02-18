7c875d11c8b75009a93b40e3c650812b62581ef6
B4si1 <153111672+B4si1@users.noreply.github.com>
2024-02-18 12:45:11 +0000

    Alert Issues

--- js/weatherscript.js
+++ js/weatherscript.js
@@ -43,7 +43,7 @@ function renderWeather(data) {
   // console.log(forecast.forecastday[0].day.daily_chance_of_rain);
   // console.log(typeof(forecast.forecastday[1].date))
   // console.log(alerts);
-  // console.log(data.alerts.alert);
+  console.log(data.alerts.alert);
 
   infoElement.innerHTML = `Last API Update : ${currentWeather.last_updated}`
 
@@ -54,7 +54,7 @@ function renderWeather(data) {
     }else{
     alertInfo.style.background = `linear-gradient(to right, transparent, red , transparent)`;
     alertInfo.style.color = `whitesmoke`;
-    alertInfo.innerHTML = `${data.alerts.alert[1].headline} : ${data.alerts.alert[1].event}`;
+    alertInfo.innerHTML = `${data.alerts.alert[0].headline} : ${data.alerts.alert[0].event}`;
   }
 
   const forecastContainer = document.getElementById("forecast");
